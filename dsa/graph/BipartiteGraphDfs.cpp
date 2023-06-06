70977dcc1ee208811602dac643913bf094a4130e
gospeller986 <palsatyajit986@gmail.com>
2023-06-07 01:57:04 +0530

    new

---
+++ dsa/graph/BipartiteGraphDfs.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+class Solution {
+public:
+    
+    bool dfs(int node , int col , vector<vector<int>>& graph , vector<int> &color  ){
+        color[node] = col ; 
+
+        for(auto it : graph[node]) {
+            if(color[it]== col) {
+                return false ; 
+            }
+            else if (color[it] == -1) {
+                if(dfs(it,!col,graph,color) == false ) return false ;
+            }
+        } 
+
+        return true ; 
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) { 
+
+        int n = graph.size() ;
+        vector<int>color(n,-1) ; 
+
+        for( int i = 0 ; i < n ; i++) {
+            if(color[i] == -1 ) {
+                if(dfs(i , 0 , graph , color) == false ) return false ; 
+            }
+        }
+
+        return true ; 
+
+
+    }
+};
+int main(){
+
+return 0 ; 
+}
